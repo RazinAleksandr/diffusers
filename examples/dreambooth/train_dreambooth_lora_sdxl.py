793a524ad011f3d14faefabef530769b81217b48
Aleksandr Razin <109418051+RazinAleksandr@users.noreply.github.com>
2025-06-26 02:04:49 +0300

    Update train_dreambooth_lora_sdxl.py

--- examples/dreambooth/train_dreambooth_lora_sdxl.py
+++ examples/dreambooth/train_dreambooth_lora_sdxl.py
@@ -1668,7 +1668,9 @@ def main(args):
             sigma = sigma.unsqueeze(-1)
         return sigma
     
-    fft_loss_fn = FFTLoss(loss_weight=0.01).to(accelerator.device)
+    # fft_loss_fn = FFTLoss(loss_weight=0.01).to(accelerator.device)
+    fft_loss_fn = FFTLoss(loss_weight=0.001).to(accelerator.device)
+
     
     for epoch in range(first_epoch, args.num_train_epochs):
         unet.train()
